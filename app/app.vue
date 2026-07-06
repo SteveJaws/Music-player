ef0eabd0151bedd6d0cda6bd58533e20830e780c
aron <aronvanduijn6363@gmail.com>
2026-07-06 21:32:27 +0200

    Refac

--- app/app.vue
+++ app/app.vue
@@ -48,6 +48,58 @@ onMounted(() => {
     artist: "testArtist",
     duration: 120,
     image: "https://picsum.photos/400"
-  })
+  });
+
+  document.addEventListener('touchstart', handleTouchStart, false);        
+  document.addEventListener('touchmove', handleTouchMove, false);
 });
+
+onUnmounted(() => {
+    // Altijd netjes opruimen om memory leaks te voorkomen
+    document.removeEventListener('touchstart', handleTouchStart);
+    document.removeEventListener('touchmove', handleTouchMove);
+});
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+        } else {
+            /* left swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            audioStore.openBubble();
+        } else { 
+            audioStore.closeBubble();
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+}
 </script>
